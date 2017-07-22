f91b0d16e8d506910b2e906f28eab496be847f96
Adrian Matejov <a.matejov@centrum.sk>
2017-07-23 00:09:42 +0200

    app: added functions from script
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- app/app.py
+++ app/app.py
@@ -7,10 +7,40 @@ app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
 assets = Environment(app)
 assets.url = app.static_url_path
-scss = Bundle('main.scss', filters='pyscss', output='main.css')
+scss = Bundle('css/main.scss', filters='pyscss', output='main.css')
 assets.register('scss_all', scss)
+
+
+def findby_num_members(num):
+    channels_response = slack.channels.list(1)
+    channels = channels_response.body['channels']
+    channels_to_archive = []
+    for channel in channels:
+        if channel['num_members'] <= num:
+            channels_to_archive.append(channel)
+    print channels_to_archive
+
+
+def findby_latest(date):
+    channels_response = slack.channels.list(1)
+    channels = channels_response.body['channels']
+    channels_to_archive = []
+    date = date_to_ts(date)
+    print date
+    for channel in channels:
+        channel_details_response = slack.channels.info(channel['id'])
+        channel_details = channel_details_response.body['channel']
+        latest_message_ts = channel_details['latest']['ts']
+        print latest_message_ts
+        if latest_message_ts < date:
+            channels_to_archive.append(channel)
+    print channels_to_archive
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.pug')
+
+
+if __name__ == '__main__':
+    app.run()
