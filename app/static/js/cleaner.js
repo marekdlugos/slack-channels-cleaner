8519d12b52e44942f9e13e3e176cba4498c70c13
Adrian Matejov <a.matejov@centrum.sk>
2017-09-26 23:24:46 +0200

    filtering by number of members
    Signed-off-by: Adrian Matejov <a.matejov@centrum.sk>

--- app/static/js/cleaner.js
+++ app/static/js/cleaner.js
@@ -3,15 +3,30 @@ function Channel(name, id, last_event, members)
     this.name = name;
     this.id = id;
     this.last_event = last_event;
-    this.members = members;
+    this.members = parseInt(members);
 }
 
-function filter_by_members(channels)
+function filter(channels, min_members, months)
 {
+    var filtered_channels = [];
 
+    for (var i = 0; i < channels.length; i++) {
+        if (channels[i].members < min_members) {
+            filtered_channels.push(channels[i]);
+        }
+    }
+
+    return filtered_channels;
 }
 
-function filter_by_ts(channels)
+function update_filtered(channels)
 {
+    var selector = $('#filtered_channels');
+    selector.empty();
 
+    for (var i = 0; i < channels.length; i++) {
+        var node = '<input type="hidden" name="' + channels[i].name + '" ' +
+                   'value="' + channels[i].id + '">';
+        selector.append(node);
+    }
 }
